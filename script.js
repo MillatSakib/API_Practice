66f104520a678d5ea5b68ab84c5830d5f027d5e3
Md. Sohan Millat Sakib <millatsakib01@gmail.com>
2024-02-27 09:27:15 +0600

    async await function added

--- script.js
+++ script.js
@@ -12,10 +12,28 @@ function userData(users) {
     const ul = document.getElementById('allUsers');
 
     for (user of users) {
-        console.log(user.name);
+        // console.log(user.name);
         const li = document.createElement('li');
         li.innerText = user.name;
         ul.appendChild(li);
 
     }
 }
+
+
+//Do the same thing with async await function
+
+
+const userDataAsinc = async () => {
+    try {
+        const res = await fetch('https://jsonplaceholder.typicode.com/comments');
+        const data = await res.json();
+        console.log(data);
+    }
+    catch (error) {
+        console.log('data load error');
+    }
+}
+
+
+userDataAsinc();
